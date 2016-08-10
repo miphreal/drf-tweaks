901e5662330b95b8c278a49be30b242ef8ba37a2
Evgeny Lychkovsky <miphreal@gmail.com>
2016-08-11 02:19:34 +0300

    updated urlpatterns

--- drf_proj/urls.py
+++ drf_proj/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls import include, url
 
 
 urlpatterns = [
-    url(r'^api/(?P<version>1\.\d{1,4}(\.\d{1,4})?)/', include(settings.API_LATEST_VERSION_URLS)),
+    # v1.x.x
+    url(r'^api/(?P<version>1\.\d{1,4}(\.\d{1,4})?)/', include('drf_proj.apps.client_api.v1.urls')),
+    # common
     url(r'^api/((?P<version>\d{1,4}(\.\d{1,4}(\.\d{1,4})?)?)/)?', include(settings.API_LATEST_VERSION_URLS)),
 ]
 
